2df040143aedf2492e9bba7dc4d0efbfdaa246a2
Jason Jun <uxd.jun@gmail.com>
2026-01-09 10:19:38 +1100

    Initial Commit

---
+++ GlimpseTests/GlimpseTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlimpseTests.swift
+//  GlimpseTests
+//
+//  Created by Glimpse Contributors on 9/1/2026.
+//
+
+import Testing
+@testable import Glimpse
+
+struct GlimpseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
